a71e27aef600231284f5308a535b0c519a6d5255
Ethan Veres <everes@butterflynetinc.com>
2019-11-14 14:13:45 -0500

    resource as abstract class

--- src/resources/Resource.ts
+++ src/resources/Resource.ts
@@ -1,17 +1,15 @@
-export default class Resource<TContext> {
+export default abstract class Resource<TContext> {
   context: TContext;
 
   constructor(context: TContext) {
     this.context = context;
   }
 
-  get(
+  abstract get(
     _id: string,
   ):
     | (Record<string, any> | null | undefined)
-    | Promise<Record<string, any> | null | undefined> {
-    throw new Error('not implemented');
-  }
+    | Promise<Record<string, any> | null | undefined>;
 
   // TODO: Once we have a concept of a model, this should go on there.
   makeId({ id }: any): string {
