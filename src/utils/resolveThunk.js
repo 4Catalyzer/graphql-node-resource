482300a73928052a243ce7760595e23a6864550f
Ethan Veres <everes@butterflynetinc.com>
2019-11-14 13:01:54 -0500

    actually remove js files

--- src/utils/resolveThunk.js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-
-import type { Thunk } from 'graphql';
-
-export default function resolveThunk<T>(thunk: Thunk<T>): T {
-  // $FlowFixMe
-  return typeof thunk === 'function' ? thunk() : thunk;
-}
