a829fe29b91a1520c2ea8cb6a0465829bb90ab05
Ethan Veres <everes@butterflynetinc.com>
2019-11-19 15:28:16 -0500

    update dataloader and remove unused test

--- test/Resource.test.ts
+++ test/Resource.test.ts
@@ -2,10 +2,7 @@ import { Resource } from '../src';
 
 describe('Resource', () => {
   it('should get make id', () => {
-    expect(new Resource({}).makeId({ id: 1 })).toEqual(1);
-  });
-
-  it('should throw for unimplemented get()', () => {
-    expect(() => new Resource({}).get('1')).toThrow(/not implemented/);
+    class ConcreteResource extends Resource<any> {}
+    expect(new ConcreteResource({}).makeId({ id: 1 })).toEqual(1);
   });
 });
